7096e8662502a812b9ec8c10cf5c4d002795709e
Artem Batutin <artembatutin@gmail.com>
2019-01-16 10:26:07 -0500

    Inbox link from profile & icons added everywhere.

--- public/js/grouplist.js
+++ public/js/grouplist.js
@@ -1,17 +1,18 @@
-$(document).ready(function()  {
+$(document).ready(function () {
     var navItems = $('.account-menu a');
     var navListItems = $('.account-menu a');
     var allWells = $('.account-content');
     var allWellsExceptFirst = $('.account-content:not(:first)');
 
     allWellsExceptFirst.hide();
-    navItems.click(function(e) {
-        e.preventDefault();
-        navListItems.removeClass('active');
-        $(this).closest('a').addClass('active');
-
-        allWells.hide();
+    navItems.click(function (e) {
         var target = $(this).attr('data-target-id');
-        $('#' + target).show();
+        if(target != null) {
+            allWells.hide();
+            e.preventDefault();
+            navListItems.removeClass('active');
+            $(this).closest('a').addClass('active');
+            $('#' + target).show();
+        }
     });
 });
